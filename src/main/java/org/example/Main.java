929bc18c1d13e675547d7489292546f8da429b7c
Gleb <hahaha>
2024-12-01 20:27:33 +0300

    updated main.java using different branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Github change 1")
+        System.out.println("Github change 1");
+	System.out.println("Change 2 with different branch in git");
     }
 }
